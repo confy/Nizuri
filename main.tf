24a44895cc56c0ed8eaea9b557a82cda9ed01cf4
Adrian McFarlane <confuzedfx@gmail.com>
2021-08-01 23:42:53 -0700

    test action trigger

--- main.tf
+++ main.tf
@@ -12,6 +12,6 @@ terraform {
 # An example resource that does nothing.
 resource "null_resource" "example" {
   triggers = {
-    value = "A example resource that does nothing!"
+    value = "A example resource that does nothing! - test trigger"
   }
 }
