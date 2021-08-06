f770a035cc57f4e9da18866f7a932cbec8207054
Adrian McFarlane <confuzedfx@gmail.com>
2021-08-06 00:30:39 -0700

    hope and pray at 1230am

--- main.tf
+++ main.tf
@@ -1,9 +1,11 @@
+// Use Aws Services with these vars
 provider "aws" {
   region     = var.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
 
+// Terraform will be run in the the cloud ("remote")
 terraform {
   backend "remote" {
     organization = "confy"
@@ -13,74 +15,148 @@ terraform {
   }
 }
 
-resource "aws_ecs_cluster" "cluster" {
-  name = "nizuri-cluster"
-}
-resource "aws_vpc" "aws-vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-}
-
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "nizuri-admin"
-
-  assume_role_policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-   {
-     "Action": "sts:AssumeRole",
-     "Principal": {
-       "Service": "ecs-tasks.amazonaws.com"
-     },
-     "Effect": "Allow",
-     "Sid": ""
-   }
- ]
-}
-EOF
-}
-resource "aws_iam_role" "ecs_task_role" {
-  name = "nizuri-admin-task"
-
-  assume_role_policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-   {
-     "Action": "sts:AssumeRole",
-     "Principal": {
-       "Service": "ecs-tasks.amazonaws.com"
-     },
-     "Effect": "Allow",
-     "Sid": ""
-   }
- ]
-}
-EOF
+// Private VPC
+resource "aws_vpc" "vpc" {
+    cidr_block = "10.0.0.0/24"
+    enable_dns_support   = true
+    enable_dns_hostnames = true
+    tags       = {
+        Name = "Nizuri_Public_VPC"
+    }
 }
 
-resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-resource "aws_iam_role_policy_attachment" "task_s3" {
-  role       = aws_iam_role.ecs_task_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+// Gateway to our VPC
+resource "aws_internet_gateway" "internet_gateway" {
+    vpc_id = aws_vpc.vpc.id
 }
 
-resource "aws_ecs_task_definition" "definition" {
-  family                = "nizuri-task-definition"
-  network_mode          = "awsvpc"
-  cpu                   = "256"
-  memory                = "1024"
-  container_definitions = <<DEFINITION
-  [
-    {
-      "image": "${var.account}.dkr.ecr.eu-west-1.amazonaws.com/nizuri:latest",
-      "name": "nizuri-container"
-      }
-    
-  ]
-  DEFINITION
+// Public Subnet
+resource "aws_subnet" "pub_subnet" {
+    vpc_id                  = aws_vpc.vpc.id
+    cidr_block              = "10.1.0.0/22"
+}
+
+// Routing table for our VPC
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.internet_gateway.id
+    }
+}
+
+// Pair public subnet with routing table
+resource "aws_route_table_association" "route_table_association" {
+    subnet_id      = aws_subnet.pub_subnet.id
+    route_table_id = aws_route_table.public.id
+}
+
+// Port Forwarding for our VPC
+resource "aws_security_group" "ecs_sg" {
+    vpc_id      = aws_vpc.vpc.id
+
+    ingress {
+        from_port       = 22
+        to_port         = 22
+        protocol        = "tcp"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port       = 80
+        to_port         = 80
+        protocol        = "tcp"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port       = 443
+        to_port         = 443
+        protocol        = "tcp"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port       = 0
+        to_port         = 65535
+        protocol        = "tcp"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
+}
+
+// Security role for our instances to use
+data "aws_iam_policy_document" "ecs_agent" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_agent" {
+  name               = "ecs-agent"
+  assume_role_policy = data.aws_iam_policy_document.ecs_agent.json
+}
+
+
+resource "aws_iam_role_policy_attachment" "ecs_agent" {
+  role       = "aws_iam_role.ecs_agent.name"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_instance_profile" "ecs_agent" {
+  name = "ecs-agent"
+  role = aws_iam_role.ecs_agent.name
+}
+
+resource "aws_launch_configuration" "ecs_launch_config" {
+    image_id             = "ami-094d4d00fd7462815"
+    iam_instance_profile = aws_iam_instance_profile.ecs_agent.name
+    security_groups      = [aws_security_group.ecs_sg.id]
+    user_data            = "#!/bin/bash\necho ECS_CLUSTER=my-cluster >> /etc/ecs/ecs.config"
+    instance_type        = "t2.micro"
+}
+
+resource "aws_autoscaling_group" "failure_analysis_ecs_asg" {
+    name                      = "asg"
+    vpc_zone_identifier       = [aws_subnet.pub_subnet.id]
+    launch_configuration      = aws_launch_configuration.ecs_launch_config.name
+
+    desired_capacity          = 2
+    min_size                  = 1
+    max_size                  = 10
+    health_check_grace_period = 300
+    health_check_type         = "EC2"
+}
+
+resource "aws_ecr_repository" "nizuri" {
+    name  = "nizuri"
+}
+
+resource "aws_ecs_cluster" "nizuri_cluster" {
+    name  = "nizuri"
+}
+
+resource "aws_ecs_task_definition" "task_definition" {
+  family                = "nizuri"
+  container_definitions = <<TASK_DEFINITION
+  {
+    "essential": true,
+    "memory": 512,
+    "name": "nizuri",
+    "cpu": 2,
+    "image": "${var.account}.dkr.${var.aws_region}-1.amazonaws.com/nizuri:latest",
+    "environment": []
+  }
+])
+}
+TASK_DEFINITION
+
+resource "aws_ecs_service" "worker" {
+  name            = "worker"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.task_definition.arn
+  desired_count   = 2
 }
