056d91e8d7f8b73e77e0997b7c00f5b700f866fb
Adrian McFarlane <confuzedfx@gmail.com>
2021-08-06 00:39:20 -0700

    change cluster name

--- main.tf
+++ main.tf
@@ -164,7 +164,7 @@ resource "aws_ecs_task_definition" "task_definition" {
 
 resource "aws_ecs_service" "worker" {
   name            = "worker"
-  cluster         = aws_ecs_cluster.ecs_cluster.id
+  cluster         = aws_ecs_cluster.nizuri_cluster.id
   task_definition = aws_ecs_task_definition.task_definition.arn
   desired_count   = 2
 }
